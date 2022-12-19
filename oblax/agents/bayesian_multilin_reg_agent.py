0f478bab0ac2d501cb42e8f44e544ac28d652934
MichelangeloConserva <mi.conserva@gmail.com>
2022-12-19 13:05:39 +0000

    bayesian linear regression agent with NormalInverseGamma model

---
+++ oblax/agents/bayesian_multilin_reg_agent.py
@@ -0,0 +1,143 @@
+from dataclasses import dataclass
+from typing import NamedTuple, Tuple, Union
+
+import chex
+import jax.numpy as jnp
+import tensorflow_probability.substrates.jax as tfp
+
+import dynamax
+from dynamax.utils.distributions import MatrixNormalInverseWishart
+from dynamax.utils.utils import psd_solve
+
+from seql.agents.base import Agent
+
+tfd = tfp.distributions
+
+
+class Info(NamedTuple):
+    ...
+
+
+# TODO: consider whether to unite the sufficient statistics with the belief state.
+
+
+@dataclass
+class BeliefState:
+    M: chex.Array
+    V: chex.Array
+    nu: float
+    Psi: chex.Array
+
+
+@dataclass
+class SufficientStatistic:
+    XX: chex.Array
+    XY: chex.Array
+    YY: chex.Array
+    n: int
+
+
+class BayesianMultivariateLinearReg(Agent):
+    @staticmethod
+    def posterior_belief(
+        prior_belief: BeliefState, suff_stats: SufficientStatistic
+    ) -> BeliefState:
+        """Compute the posterior belief of the Bayesian linear regression agent from a given prior and sufficient statistics.
+
+        Args:
+            prior_belief: the initial belief of the agent.
+            suff_stats: the sufficient statistics of the observed data.
+
+        Returns:
+            The updated belief of the agent.
+        """
+
+        Sxx = prior_belief.V + suff_stats.XX
+        Sxy = suff_stats.XY + prior_belief.V @ prior_belief.M.T
+        Syy = suff_stats.YY + prior_belief.M @ prior_belief.V @ prior_belief.M.T
+        M_pos = psd_solve(Sxx, Sxy).T
+        V_pos = Sxx
+        nu_pos = prior_belief.nu + suff_stats.n
+        Psi_pos = prior_belief.Psi + Syy - M_pos @ Sxy
+
+        return BeliefState(M_pos, V_pos, nu_pos, Psi_pos)
+
+    def __init__(self, prior_belief: BeliefState, is_classifier: bool = False):
+        super(BayesianMultivariateLinearReg, self).__init__(is_classifier)
+
+        self.m, self.d = prior_belief.M.shape
+
+        self.prior_belief = prior_belief
+        self.suff_stats = SufficientStatistic(
+            XX=jnp.zeros((self.d, self.d)),
+            XY=jnp.zeros((self.d, self.m)),
+            YY=jnp.zeros((self.m, self.m)),
+            n=0,
+        )
+
+        self.model_fn = lambda params, x: x @ params.T
+        self._posterior_belief = None
+
+    def init_state(
+        self, M: chex.Array, V: chex.Array, nu: float, Psi: chex.Array
+    ) -> BeliefState:
+        """Return the object encoding the given belief."""
+        return BeliefState(M, V, nu, Psi)
+
+    def _update_suff_stats(self, x: chex.Array, y: chex.Array):
+        self.suff_stats.XX += x.T @ x
+        self.suff_stats.XY += (x.T @ y).squeeze()
+        self.suff_stats.YY += y.T @ y
+        self.suff_stats.n += len(x)
+
+    def update(
+        self,
+        key: chex.PRNGKey,
+        belief: Union[BeliefState, None],
+        x: chex.Array,
+        y: chex.Array,
+        return_posterior: bool = True,
+    ) -> Union[Tuple[BeliefState, Info], None]:
+        """Update the agent sufficient statistics given some observed data.
+
+        Args:
+            key: the random key. It is not used for this agent.
+            belief: the starting belief of the agent. When set to None the agent will use its prior belief.
+            x: the observed covariates.
+            y: the observed variates.
+            return_posterior: whether to return the posterior belief.
+
+        Returns:
+            If required it returns the posterior belief.
+        """
+
+        if belief is None:
+            belief = self.prior_belief
+
+        x = x.reshape(-1, self.d)
+        y = y.reshape(-1, self.m)
+
+        self._update_suff_stats(x, y)
+
+        if return_posterior:
+            posterior_belief = BayesianMultivariateLinearReg.posterior_belief(
+                belief, self.suff_stats
+            )
+            return posterior_belief, Info()
+
+    def sample_params(self, key: chex.PRNGKey, belief: BeliefState) -> chex.ArrayTree:
+        """Sample parameters from the given belief."""
+        mniw = MatrixNormalInverseWishart(belief.M, belief.V, belief.nu, belief.Psi)
+        Sigma_samples, Matrix_samples = mniw.sample(seed=key)
+        return Sigma_samples, Matrix_samples
+
+    def predict_given_params_regression(self, params: chex.ArrayTree, x: chex.Array):
+        """Predict the value of the given covariates."""
+        Sigma_samples, Matrix_samples = params
+        pred = self.model_fn(Matrix_samples, x)
+
+        # n test examples, dimensionality of output
+        # pred_dist = distrax.MultivariateNormalDiag(loc=pred, covariance=sigma_squared * jnp.eye(len(pred))) # the output shape is (len(pred), len(pred)) for some reason here
+        pred_dist = tfd.MultivariateNormalFullCovariance(pred, Sigma_samples)
+
+        return pred_dist
---
+++ oblax/agents/bayesian_multilin_reg_agent_test.py
@@ -0,0 +1,167 @@
+import itertools
+
+import chex
+import jax
+import jax.numpy as jnp
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import tensorflow_probability.substrates.jax as tfp
+from dynamax.utils.distributions import InverseWishart
+
+from oblax.agents.bayesian_multilin_reg_agent import (
+    BayesianMultivariateLinearReg,
+    BeliefState,
+)
+from oblax.environments.sequential_2d import linear_regression2d_rotation
+
+
+tfd = tfp.distributions
+
+
+class BayesMultiLinRegTest(parameterized.TestCase):
+    @parameterized.parameters(itertools.product([2, 10, 50], [2, 10, 50]))
+    def test_init_state(self, d: int, m: int):
+
+        loc = jnp.ones((m, d))
+        col_precision = jnp.eye(d)
+        df = m + 1.0
+        scale = jnp.eye(m)
+        agent = BayesianMultivariateLinearReg(
+            BeliefState(loc, col_precision, df, scale)
+        )
+
+        prior_belief = agent.init_state(loc, col_precision, df, scale)
+        prior_belief1 = agent.prior_belief
+
+        chex.assert_shape(prior_belief.M, (m, d))
+        chex.assert_shape(prior_belief.V, (d, d))
+        chex.assert_shape(prior_belief.Psi, (m, m))
+        chex.assert_type(prior_belief.nu, float)
+
+        chex.assert_shape(prior_belief1.M, (m, d))
+        chex.assert_shape(prior_belief1.V, (d, d))
+        chex.assert_shape(prior_belief1.Psi, (m, m))
+        chex.assert_type(prior_belief1.nu, float)
+
+    @parameterized.parameters(
+        itertools.product((4, 10), (2000, 1000), [2, 10], [2, 10])
+    )
+    def test_update(self, seed: int, N: int, d: int, m: int):
+
+        x_key, w_key, noise_key, logprob_key = jax.random.split(
+            jax.random.PRNGKey(seed), 4
+        )
+        x = jax.random.normal(x_key, shape=(N, d))
+        w = jax.random.normal(w_key, shape=(d, m))
+        y = x @ w + jax.random.normal(noise_key, (N, m))
+
+        loc = jnp.ones((m, d))
+        col_precision = jnp.eye(d)
+        df = m + 1.0
+        scale = jnp.eye(m)
+        agent = BayesianMultivariateLinearReg(
+            BeliefState(loc, col_precision, df, scale)
+        )
+
+        posterior_belief, _ = agent.update(None, None, x, y)
+
+        # Check if the posterior mean is close to the true parameter
+        assert jnp.isclose(w, posterior_belief.M.T, 1e-1, 1e-1).all()
+
+        # Check if the posterior variance mean is close to the true observation noise
+        assert jnp.isclose(
+            jnp.eye(m),
+            InverseWishart(posterior_belief.nu, posterior_belief.Psi).mean(),
+            1e-1,
+            1e-1,
+        ).all()
+
+    @parameterized.parameters(itertools.product((4, 10), (10, 25), (10, 25)))
+    def test_sample_params(self, seed: int, d: int, m: int):
+        loc = jnp.ones((m, d))
+        col_precision = jnp.eye(d)
+        df = m + 1.0
+        scale = jnp.eye(m)
+        agent = BayesianMultivariateLinearReg(
+            BeliefState(loc, col_precision, df, scale)
+        )
+        key = jax.random.PRNGKey(seed)
+
+        Sigma_samples, Matrix_samples = agent.sample_params(key, agent.prior_belief)
+        chex.assert_shape(Matrix_samples, (m, d))
+        chex.assert_shape(Sigma_samples, (m, m))
+
+    @parameterized.parameters(itertools.product((0,), (10,), (2,), (10,), (5,), (4,)))
+    def test_posterior_predictive_sample(
+        self,
+        seed: int,
+        ntrain: int,
+        input_dim: int,
+        nsamples_params: int,
+        nsamples_output: int,
+        output_dim: int,
+    ):
+        m, d, = (
+            output_dim,
+            input_dim,
+        )
+
+        loc = jnp.ones((m, d))
+        col_precision = jnp.eye(d)
+        df = m + 1.0
+        scale = jnp.eye(m)
+        agent = BayesianMultivariateLinearReg(
+            BeliefState(loc, col_precision, df, scale)
+        )
+
+        key = jax.random.PRNGKey(seed)
+        x_key, ppd_key = jax.random.split(key)
+
+        x = jax.random.normal(x_key, shape=(ntrain, input_dim))
+        samples = agent.posterior_predictive_sample(
+            key, agent.prior_belief, x, nsamples_params, nsamples_output
+        )
+        chex.assert_shape(
+            samples, (nsamples_params, ntrain, nsamples_output, output_dim)
+        )
+
+    @parameterized.parameters(itertools.product((0,), (5,), (2,), (10,), (5,)))
+    def test_logprob_given_belief(
+        self,
+        seed: int,
+        ntrain: int,
+        input_dim: int,
+        nsamples_params: int,
+        output_dim: int,
+    ):
+        m, d, = (
+            output_dim,
+            input_dim,
+        )
+
+        loc = jnp.ones((m, d))
+        col_precision = jnp.eye(d)
+        df = m + 1.0
+        scale = jnp.eye(m)
+        agent = BayesianMultivariateLinearReg(
+            BeliefState(loc, col_precision, df, scale)
+        )
+
+        key = jax.random.PRNGKey(seed)
+        x_key, w_key, noise_key, logprob_key = jax.random.split(key, 4)
+
+        x = jax.random.normal(x_key, shape=(ntrain, input_dim))
+        w = jax.random.normal(w_key, shape=(input_dim, output_dim))
+        y = x @ w + jax.random.normal(noise_key, (ntrain, output_dim))
+
+        samples = agent.logprob_given_belief(
+            logprob_key, agent.prior_belief, x, y, nsamples_params
+        )
+        chex.assert_shape(samples, (ntrain, 1))
+        assert not jnp.any(jnp.isinf(samples))
+        assert not jnp.any(jnp.isnan(samples))
+
+
+if __name__ == "__main__":
+    absltest.main()
